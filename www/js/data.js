3326285230d3630ea60e809b8c51f5a203a11187
blorpslurp <i_gargle_good3@protonmail.com>
2025-03-15 12:15:46 -0400

    Update data.js
    updated rank values to correct values

--- www/js/data.js
+++ www/js/data.js
@@ -253,10 +253,10 @@ if (typeof JSPREF !== "object" || JSPREF === null) {
 var Rank = {
     Guest: 0,
     Member: 1,
-    Leader: 19,
-    Moderator: 20,
-    Admin: 30,
-    Owner: 36,
+    Leader: 4.5,
+    Moderator: 5,
+    Admin: 8,
+    Owner: 10,
     Siteadmin: 255
 };
 
